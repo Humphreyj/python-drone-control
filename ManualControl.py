6ee001e10f1a8782554a074181a13773abbb50f2
Joshua Humphrey <joshhumphrey1@gmail.com>
2021-07-19 11:34:02 -0500

    manual control achieved

--- ManualControl.py
+++ ManualControl.py
@@ -8,6 +8,7 @@ global image
 kp.init()
 drone = tello.Tello()
 drone.connect()
+drone.streamon()
 centimers_per_foot = 30.48
 print(f'{drone.get_battery()} % remaining')
 
@@ -15,12 +16,13 @@ print(f'{drone.get_battery()} % remaining')
 def getKeyboardInput():
     left_right, forward_back, up_down, yaw = 0, 0, 0, 0
 
-    speed = 40
+    speed = 50
+    strafe_speed = 50
     # lateral movement
     if kp.getKey("a"):
-        left_right = -speed
+        left_right = -strafe_speed
     if kp.getKey("d"):
-        left_right = speed
+        left_right = strafe_speed
         # yaw
     if kp.getKey("q"):
         yaw = -speed
@@ -38,6 +40,7 @@ def getKeyboardInput():
         up_down = speed
 
     # landing
+
     if kp.getKey("n"):
         drone.land()
         time.sleep(5)
@@ -52,6 +55,9 @@ def getKeyboardInput():
     # get height
     if kp.getKey("h"):
         print(f'Altitude is {drone.get_height() / centimers_per_foot} feet.')
+    # end flight
+    if kp.getKey("i"):
+        drone.end()
     return [left_right, forward_back, up_down, yaw]
 
 
@@ -61,6 +67,6 @@ while True:
 
     drone.send_rc_control(values[0], values[1], values[2], values[3])
     image = drone.get_frame_read().frame
-    image = cv2.resize(image, (360, 306))
+    image = cv2.resize(image, (1280, 720))
     cv2.imshow("Drone Feed", image)
     cv2.waitKey(1)
